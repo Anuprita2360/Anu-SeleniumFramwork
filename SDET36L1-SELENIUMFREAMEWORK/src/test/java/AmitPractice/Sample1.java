19ab7fb19ae37426b0a6b791d93c52f888318418
hp <hp@AMIT-PC>
2022-09-26 11:35:36 +0530

    Amit commit 1st

---
+++ SDET36L1-SELENIUMFREAMEWORK/src/test/java/AmitPractice/Sample1.java
@@ -0,0 +1,13 @@
+package AmitPractice;
+
+import org.testng.annotations.Test;
+
+public class Sample1 
+{
+	@Test
+	public void display()
+	{
+		System.out.println("Amit modified this Scriot");
+	}
+
+}
